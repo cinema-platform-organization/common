b02e5be541925e84329453a386b41363e511853c
Ruslan Parkhomchuk <parkhomchuk.lol@gmail.com>
2026-08-31 19:44:51 +0300

    Unify gRPC clients and modules

---
+++ lib/grpc/decorators/inject-grpc-client.decorator.ts
@@ -0,0 +1,7 @@
+import { Inject } from "@nestjs/common";
+
+import { GRPC_CLIENT_PREFIX } from "../constants/grpc.constants";
+
+export function InjectGrpcClient(name: string) {
+	Inject(`${GRPC_CLIENT_PREFIX}_${name}`);
+}
