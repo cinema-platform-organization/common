47e5d2d85ee8d3521293094b68e6c3fcf3cd1db3
Ruslan Parkhomchuk <parkhomchuk.lol@gmail.com>
2026-09-06 23:37:10 +0300

    Add theater package to registry

--- lib/grpc/registry/grpc.registry.ts
+++ lib/grpc/registry/grpc.registry.ts
@@ -31,4 +31,9 @@ export const GRPC_CLIENTS = {
 		protoPath: PROTO_PATHS.CATEGORY,
 		env: "MOVIE_GRPC_URL",
 	},
+	THEATER_PACKAGE: {
+		package: "theater.v1",
+		protoPath: PROTO_PATHS.THEATER,
+		env: "MOVIE_GRPC_URL",
+	},
 } as const;
